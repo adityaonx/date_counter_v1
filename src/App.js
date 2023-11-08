5f4c5e0fdbb5f7c9047cfb5fa9c485646b0c5e11
adityaonx <adityaonx@gmail.com>
2023-11-09 03:05:40 +0530

    Date counter with proper use of state to update values like count,steps and date.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  let vals;
+  let today = new Date();
+  let newDate = new Date(today);
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+  const [date, setDate] = useState(today);
+  function calcDate() {
+    setDate((d) => {
+      console.log(d);
+      newDate = new Date(d);
+      newDate.setDate(newDate.getDate() - step);
+      return newDate;
+    });
+  }
+  function stepPrevious() {
+    if (step > 1) setStep((s) => s - 1);
+  }
+  function stepForward() {
+    setStep((s) => s + 1);
+  }
+  function countPrevious() {
+    if (count > 0) {
+      setCount((c) => {
+        vals = c - step;
+        return vals;
+      });
+      calcDate();
+    }
+  }
+  function countForward() {
+    setCount((c) => {
+      vals = c + step;
+      newDate = new Date(today);
+      newDate.setDate(newDate.getDate() + vals);
+      setDate((d) => newDate);
+      return vals;
+    });
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="step">
+        <button onClick={stepPrevious}>-</button>
+        Step: {step}
+        <button onClick={stepForward}>+</button>
+      </div>
+      <div className="count">
+        <button onClick={countPrevious}>-</button>Count: {count}
+        <button onClick={countForward}>+</button>
+      </div>
+      <div className="summary">
+        <h4>
+          {count} days from today is {date.toDateString()}
+        </h4>
+      </div>
     </div>
   );
 }
